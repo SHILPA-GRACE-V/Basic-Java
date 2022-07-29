c0dee0b33ccfcfeba911a83ed22811cdb49efb6d
SHILPA-GRACE-V <sgv3009@gmail.com>
2022-07-29 11:07:06 +0530

    Fibinocci Serie

---
+++ src/Fibinocci.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Fibinocci {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int x1=0 , x2=1, x3;
+
+        System.out.println("Enter the count to be printed:");
+        int count = sc.nextInt();
+
+        System.out.print(x1+" "+x2);
+
+        for (int i=0; i<count; i++){
+            x3= x1+x2;
+            System.out.print(" "+x3);
+            x1=x2;
+            x2=x3;
+
+        }
+
+    }
+}
